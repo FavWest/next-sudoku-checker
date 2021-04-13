6dc982d14a56c9e58e7037e81a00055995dc16e4
Tori FavWest <practiceandthenpractice@gmail.com>
2021-04-13 15:17:56 -0700

    pass test: checkRows returns false if numbers are missing
    Co-authored-by: Noah Woodrum <heymymomthinksimcool@gmail.com>

--- src/sudoku.js
+++ src/sudoku.js
@@ -3,9 +3,11 @@ Sudoku(grid){
   this.grid=grid;
 }
 Sudoku.prototype.checkRows= function (){
-  for(let i=1; i<10; i++){
-    if(!this.grid[0].includes(i)){
-      return false;
+  for(let row=0; row<9; row++){
+    for(let i=1; i<10; i++){
+      if(!this.grid[row].includes(i)){
+        return false;
+      }
     }
   }
   return true;
